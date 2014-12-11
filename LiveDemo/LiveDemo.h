7a77d9608933853b9a2483fc75a33e0d7d9fd785
Anthony Mittaz <anthony@lifx.co>
2014-12-11 12:34:10 +1100

    added LiveDemo framework, embedded into app

---
+++ LiveDemo/LiveDemo.h
@@ -0,0 +1,19 @@
+//
+//  LiveDemo.h
+//  LiveDemo
+//
+//  Created by Anthony Mittaz on 11/12/2014.
+//  Copyright (c) 2014 LIFX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiveDemo.
+FOUNDATION_EXPORT double LiveDemoVersionNumber;
+
+//! Project version string for LiveDemo.
+FOUNDATION_EXPORT const unsigned char LiveDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveDemo/PublicHeader.h>
+
+
